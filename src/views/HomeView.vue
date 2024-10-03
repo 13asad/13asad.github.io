e54a1b9762133f00112b059eea9a71e01990a82c
Mohammad Asad Atterkhan <94637256+ma-asad@users.noreply.github.com>
2024-10-03 13:39:09 +0400

    feat:

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <script setup>
 import HelloWorld from '@/components/HelloWorld.vue';
-// import Post from '@/components/Post.vue';
-// import Projects from '@/components/Projects.vue';
-import SVL from '@/components/SVL.vue';
+import Post from '@/components/Post.vue';
+import Projects from '@/components/Projects.vue';
+// import SVL from '@/components/SVL.vue';
 </script>
 
 <template>
   <HelloWorld />
-  <!-- <Post />
-  <Projects /> -->
-  <SVL />
+  <Post />
+  <Projects />
+  <!-- <SVL /> -->
 </template>
