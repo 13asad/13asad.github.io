724d5faf435002c463a95151713c85a995b2206f
Mohammad Asad Atterkhan <94637256+ma-asad@users.noreply.github.com>
2024-10-03 13:39:38 +0400

    feat: re-added post and projects component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,11 @@
 import HelloWorld from '@/components/HelloWorld.vue';
 import Post from '@/components/Post.vue';
 import Projects from '@/components/Projects.vue';
-// import SVL from '@/components/SVL.vue';
+
 </script>
 
 <template>
   <HelloWorld />
   <Post />
   <Projects />
-  <!-- <SVL /> -->
 </template>
