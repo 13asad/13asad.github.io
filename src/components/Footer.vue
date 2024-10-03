db7a55ea7e3a938450f86ac7fdf137f30fe7ca07
Mohammad Asad Atterkhan <94637256+ma-asad@users.noreply.github.com>
2024-10-03 08:52:56 +0400

    feat: responsive footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,15 +3,15 @@ const currentYear = new Date().getFullYear();
 </script>
 
 <template>
-  <footer class="py-1">
-    <div class="container mx-auto px-4 flex justify-between items-center">
-      <p>&copy; {{ currentYear }} Mohammad Asad Atterkhan</p>
+  <footer class="py-4">
+    <div class="container mx-auto px-4 flex flex-col items-center">
+      <p class="mb-2">&copy; {{ currentYear }} Mohammad Asad Atterkhan</p>
       <nav>
-        <ul class="flex space-x-4">
-            <li><RouterLink to="/" class="hover:underline">Home</RouterLink></li>
-            <li><RouterLink to="/about" class="hover:underline">About</RouterLink></li>
-            <li><RouterLink to="/blog" class="hover:underline">Blog</RouterLink></li>
-            <li><RouterLink to="/projects" class="hover:underline">Projects</RouterLink></li>
+        <ul class="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4">
+          <li><RouterLink to="/" class="hover:underline">Home</RouterLink></li>
+          <li><RouterLink to="/about" class="hover:underline">About</RouterLink></li>
+          <li><RouterLink to="/blog" class="hover:underline">Blog</RouterLink></li>
+          <li><RouterLink to="/projects" class="hover:underline">Projects</RouterLink></li>
         </ul>
       </nav>
     </div>
