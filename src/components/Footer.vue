9c2e8f6e29bb9ad27f8b0f972456f882cad898da
Mohammad Asad Atterkhan <94637256+ma-asad@users.noreply.github.com>
2024-10-14 01:13:59 +0400

    refactor: update Footer.vue component
    - Update footer layout and styling
    - Add social links component
    - Improve code readability and organization

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,19 @@
 <script setup>
+import SocialLinks from '@/components/SocialLinks.vue';
+
 const currentYear = new Date().getFullYear();
 </script>
 
 <template>
-  <footer class="py-4">
-    <div class="container mx-auto px-4 flex flex-col items-center">
-      <p class="mb-2">&copy; {{ currentYear }} Mohammad Asad Atterkhan</p>
-      <nav>
-        <ul class="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4">
-          <li><RouterLink to="/" class="hover:underline">Home</RouterLink></li>
-          <li><RouterLink to="/about" class="hover:underline">About</RouterLink></li>
-          <li><RouterLink to="/blog" class="hover:underline">Blog</RouterLink></li>
-          <li><RouterLink to="/projects" class="hover:underline">Projects</RouterLink></li>
-        </ul>
-      </nav>
+  <footer class="py-4 text-gray-700">
+    <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0">
+      <p class="mb-2 md:mb-0 text-center md:text-left">
+        &copy; {{ currentYear }} Mohammad Asad Atterkhan
+      </p>
+
+      <div class="flex justify-center md:justify-end mt-2 md:mt-0">
+        <SocialLinks />
+      </div>
     </div>
   </footer>
 </template>
