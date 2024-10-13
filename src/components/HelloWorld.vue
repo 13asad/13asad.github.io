7725c5d9189ce2804a248abb4880a5604a115e3c
Mohammad Asad Atterkhan <94637256+ma-asad@users.noreply.github.com>
2024-10-14 00:54:28 +0400

    refactor: update greeting and add full name in HelloWorld.vue component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,41 +4,17 @@ import SocialLinks from '@/components/SocialLinks.vue'
 
 <template>
   <section class="mb-12 bg-blue-50 p-4 rounded-lg">
-    <h2 class="text-3xl font-bold mb-4">Hi, I'm Asad! ^-^</h2>
+    <h2 class="text-3xl font-bold mb-4">Hi, I'm Mohammad Asad Atterkhan!👋</h2>
     <p class="mb-4">
-      I'm <b>Mohammad Asad Atterkhan</b>. I'm currently a final year Computer Science & Systems Engineering
-      (CSSE) student and also an IT Engineer. I have a passion for solving problems and tweaking
-      systems. Outside of work, I enjoy exploring other fields and work on personal projects. Oh,
-      and I love cats!
+      Currently working as an IT Engineer at <a href="https://www.cieltextile.com" target="_blank" class="text-blue-700 hover:underline">Tropic</a>.
+    </p>
+    <p class="mb-4">
+      Passionate about solving problems and optimizing systems. Enthusiast of technology and innovation.<br>
+      Oh and I like Cats! 🐱
+    </p>
+    <p class="mb-4">
+      
     </p>
     <SocialLinks />
   </section>
 </template>
-
-<!-- <script>
-export default {
-  data() {
-    return {
-      posts: [
-        { date: '14 Aug 2023', title: 'Example Cover Image', coverImage: '@/assets/img/image.jpg' },
-        { date: '22 Feb 2023', title: 'A post of Markdown elements', coverImage: '@/assets/img/image.jpg' },
-        { date: '22 Feb 2023', title: 'This post doesn\'t have any content', coverImage: '@/assets/img/image.jpg' },
-        { date: '1 Feb 2023', title: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id', coverImage: '@/assets/img/image.jpg' },
-        { date: '30 Jan 2023', title: 'Unique tags validation', coverImage: '@/assets/img/image.jpg' },
-        { date: '27 Jan 2023', title: 'Example Social Image', coverImage: '@/assets/img/image.jpg' },
-        { date: '12 Jun 2022', title: 'My Second Post', coverImage: '@/assets/img/image.jpg' },
-        { date: '13 May 2022', title: 'Testing testing 123!', coverImage: '@/assets/img/image.jpg' },
-      ],
-      technologies: [
-        { name: 'Astro', description: 'Build fast websites, faster.' },
-        { name: 'Astro Assets', description: 'Built-in optimized asset support.' },
-        { name: 'Tailwind CSS', description: 'Rapidly build modern websites without ever leaving your HTML.' },
-        { name: 'Markdown', description: 'Simple and easy-to-use markup language.' },
-        { name: 'MDX', description: 'Markdown for the component era.' },
-        { name: 'Satori', description: 'Generating png Open Graph images for blog posts.' },
-        { name: 'Pagefind', description: 'A fully static search library.' },
-      ],
-    };
-  },
-};
-</script> -->
