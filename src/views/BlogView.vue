f65f481e3f063c10fd0d35b97c508c5cfc88ee08
Mohammad Asad Atterkhan <94637256+ma-asad@users.noreply.github.com>
2024-10-14 22:11:54 +0400

    feat: Display all blog posts on the BlogView page

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,7 +1,13 @@
 <script setup>
-import UnderConstruction from '@/components/UnderConstruction.vue';
+import PostCard from '@/components/Posts/PostCard.vue';
+import { posts } from '@/data/postData';
 </script>
 
 <template>
-  <UnderConstruction />
+  <section class="mb-12">
+    <h2 class="text-2xl font-bold mb-4">All Posts</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <PostCard v-for="post in posts" :key="post.date" :post="post" />
+    </div>
+  </section>
 </template>
