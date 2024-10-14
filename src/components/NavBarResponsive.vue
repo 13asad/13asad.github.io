6bc6bee1e67523776e77e0729ad5444a9b9965b3
Mohammad Asad Atterkhan <94637256+ma-asad@users.noreply.github.com>
2024-10-14 23:25:46 +0400

    refactor: Update NavBarResponsive.vue to improve header behavior and styling

--- src/components/NavBarResponsive.vue
+++ src/components/NavBarResponsive.vue
@@ -1,11 +1,10 @@
+<!-- NavBarResponsive.vue -->
 <script setup>
 import { RouterLink } from 'vue-router';
 import { ref, onMounted, onUnmounted } from 'vue';
 
 const menuOpen = ref(false);
 const isFixed = ref(false);
-const isVisible = ref(true);
-let lastScrollY = 0;
 
 function toggleMenu() {
   menuOpen.value = !menuOpen.value;
@@ -14,21 +13,8 @@ function toggleMenu() {
 const handleScroll = () => {
   const currentScrollY = window.scrollY;
   
-  // Determine if header should be fixed
+  // make header fixed after scrolling 50px
   isFixed.value = currentScrollY > 50;
-
-  // Determine visibility based on scroll direction, but only when fixed
-  if (isFixed.value) {
-    if (currentScrollY < lastScrollY) {
-      isVisible.value = true;
-    } else {
-      isVisible.value = false;
-    }
-  } else {
-    isVisible.value = true;
-  }
-  
-  lastScrollY = currentScrollY;
 };
 
 onMounted(() => {
@@ -46,41 +32,39 @@ onUnmounted(() => {
       'w-full bg-white bg-opacity-90 z-50 transition-all duration-300 px-6 sm:px-8 md:px-16',
       { 'fixed top-0 left-0 shadow-md': isFixed },
       { 'relative': !isFixed },
-      { 'transform -translate-y-full': !isVisible && isFixed },
       { 'py-2': isFixed, 'py-4': !isFixed }
     ]"
   >
-    <div class="flex flex-col items-center">
-      <div class="flex flex-col items-center space-y-2">
+    <div :class="[{ 'flex items-center justify-between w-full': isFixed, 'flex flex-col items-center': !isFixed }]">
+      <div :class="[{ 'flex items-center space-x-2': isFixed, 'flex flex-col items-center space-y-2': !isFixed }]">
         <img 
           src="@/assets/img/PixMeAlone.gif" 
           alt="Pixel Me" 
-          :class="{ 'h-14 w-14': isFixed, 'h-15 w-15': !isFixed }"
+          :class="{ 'h-10 w-10': isFixed, 'h-15 w-15': !isFixed }"
         />
       </div>
       
       <!-- Centered Hamburger Icon -->
-      <button class="md:hidden block mt-2" @click="toggleMenu">
+      <button class="md:hidden block mt-2" @click="toggleMenu" :class="{ 'mt-0': isFixed }">
         <font-awesome-icon :icon="['fas', 'ellipsis']" size="2x"/>
       </button>
-
-      <!-- Navigation -->
-      <nav 
-        :class="{ 
-          'hidden': !menuOpen, 
-          'block': menuOpen,
-          'md:block': true // Always show on larger screens
-        }" 
-        class="w-full mt-4 md:mt-2"
-      >
-        <ul class="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4">
-          <li><RouterLink to="/" class="hover:underline hover:text-red-700">Home</RouterLink></li>
-          <li><RouterLink to="/about" class="hover:underline hover:text-red-700">About</RouterLink></li>
-          <li><RouterLink to="/blog" class="hover:underline hover:text-red-700">Blog</RouterLink></li>
-          <li><RouterLink to="/projects" class="hover:underline hover:text-red-700">Projects</RouterLink></li>
-        </ul>
-      </nav>
     </div>
+
+    <!-- Navigation -->
+    <nav 
+      :class="{ 
+        'hidden': !menuOpen, 
+        'block': menuOpen
+      }" 
+      class="w-full mt-4 md:mt-2"
+    >
+      <ul class="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4">
+        <li><RouterLink to="/" class="hover:underline hover:text-red-700">Home</RouterLink></li>
+        <li><RouterLink to="/about" class="hover:underline hover:text-red-700">About</RouterLink></li>
+        <li><RouterLink to="/blog" class="hover:underline hover:text-red-700">Blog</RouterLink></li>
+        <li><RouterLink to="/projects" class="hover:underline hover:text-red-700">Projects</RouterLink></li>
+      </ul>
+    </nav>
   </header>
   
   <!-- Spacer div to prevent content jump when header becomes fixed -->
