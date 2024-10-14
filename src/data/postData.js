e64c5979ca0b3ce9491a9768e6de818ca5017b85
Mohammad Asad Atterkhan <94637256+ma-asad@users.noreply.github.com>
2024-10-15 00:04:59 +0400

    refactor: Update coverImage URLs in postData.js

--- src/data/postData.js
+++ src/data/postData.js
@@ -4,36 +4,36 @@ export const posts = [
   {
     date: '2023-10-01',
     title: 'Introduction to Python',
-    coverImage: 'https://via.placeholder.com/150'
+    coverImage: 'assets/img/post/150.png'
   },
   {
     date: '2023-09-25',
     title: 'Understanding Cybersecurity',
-    coverImage: 'https://via.placeholder.com/150'
+    coverImage: 'assets/img/post/150.png'
   },
   {
     date: '2023-09-15',
     title: 'Setup A Windows Server',
-    coverImage: 'https://via.placeholder.com/150'
+    coverImage: 'assets/img/post/150.png'
   },
   {
     date: '2023-09-10',
     title: 'AI and Machine Learning Basics',
-    coverImage: 'https://via.placeholder.com/150'
+    coverImage: 'assets/img/post/150.png'
   },
   {
     date: '2023-09-05',
     title: 'Networking Fundamentals',
-    coverImage: 'https://via.placeholder.com/150'
+    coverImage: 'assets/img/post/150.png'
   },
   {
     date: '2023-09-01',
     title: 'AD for Beginners',
-    coverImage: 'https://via.placeholder.com/150'
+    coverImage: 'assets/img/post/150.png'
   },
   {
     date: '2023-08-25',
     title: 'Setup Your Ubuntu Server',
-    coverImage: 'https://via.placeholder.com/150'
+    coverImage: 'assets/img/post/150.png'
   }
-];
+]
