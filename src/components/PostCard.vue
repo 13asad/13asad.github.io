b3b9bef61a71c48918a4625b3818af009c6e76f4
Mohammad Asad Atterkhan <94637256+ma-asad@users.noreply.github.com>
2025-01-08 00:42:15 +0400

    refactor: enhance PostCard layout for improved readability and responsiveness. making all cards fixed size.

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { defineProps } from 'vue'
+
 const props = defineProps({
   post: {
     type: Object,
@@ -7,25 +8,36 @@ const props = defineProps({
   }
 })
 </script>
+
 <template>
   <div
-    class="bg-white shadow-md rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 w-11/12 md:w-full mx-auto"
+    class="bg-white shadow-md rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 w-60 h-64 mx-auto"
   >
-    <img :src="props.post.coverImage" :alt="props.post.title" class="w-full h-48 object-cover" />
-    <div class="p-4">
-      <span class="text-gray-500">{{ props.post.date }}</span>
-      <router-link
-        :to="`/post/${props.post.id}`"
-        class="block text-xl font-semibold hover:underline mt-2"
-      >
-        {{ props.post.title }}
-      </router-link>
-      <div class="mt-2">
-        <span class="text-gray-500">{{ props.post.categories.join(', ') }}</span>
+    <img
+      :src="props.post.coverImage"
+      :alt="props.post.title"
+      class="w-full h-32 object-cover"
+    />
+    <div class="p-4 flex flex-col justify-between h-full">
+      <div>
+        <span class="text-gray-500 text-sm">{{ props.post.date }}</span>
+        <router-link
+          :to="`/post/${props.post.id}`"
+          class="block text-xl font-semibold hover:underline mt-2 truncate"
+        >
+          {{ props.post.title }}
+        </router-link>
+        <div class="mt-2">
+          <span class="text-gray-500 text-sm">{{ props.post.categories.join(', ') }}</span>
+        </div>
+      </div>
+      <div>
+        <!-- Additional content can go here if needed -->
       </div>
     </div>
   </div>
 </template>
+
 <style scoped>
 .grid {
   display: grid;
