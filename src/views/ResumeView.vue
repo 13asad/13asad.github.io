1bbd7725e2a05bcdc0d1d66de2628266d7e67a2b
Mohammad Asad Atterkhan <94637256+ma-asad@users.noreply.github.com>
2025-01-08 02:53:15 +0400

    feat: integrate ResumeEducation and ResumeCerts components into ResumeView with download link for PDF

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -2,14 +2,22 @@
 import ResumeAbout from '@/components/resume/ResumeAbout.vue'
 import ResumeSkills from '@/components/resume/ResumeSkills.vue'
 import ResumeWorkExp from '@/components/resume/ResumeWorkExp.vue';
+import ResumeEducation from '@/components/resume/ResumeEducation.vue';
+import ResumeCerts from '@/components/resume/ResumeCerts.vue';
 </script>
 
 <template>
   <div class="bg-[#fdfcfb] p-6">
+    <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" class="mb-4 p-2 rounded underline flex items-center inline-block">
+      <font-awesome-icon :icon="['fas', 'download']" class="mr-2" />
+      Download PDF
+    </a>
     <section>
       <ResumeAbout />
       <ResumeSkills />
       <ResumeWorkExp />
+      <ResumeEducation />
+      <ResumeCerts />
     </section>
   </div>
 </template>
